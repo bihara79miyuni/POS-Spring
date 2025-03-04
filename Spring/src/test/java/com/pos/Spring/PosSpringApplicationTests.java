a5687d248ff02b40c2afb0585c1a509b35d192d8
bihara79miyuni <biharamiyuni@gmail.com>
2025-03-04 14:41:56 +0530

    initial commit

---
+++ Spring/src/test/java/com/pos/Spring/PosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
